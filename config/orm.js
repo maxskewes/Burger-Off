b1cd2c6ab9eedc600fe01211ba2f037a2a3e5612
Max <maxskewes@gmail.com>
2020-02-04 14:43:44 -0800

    All folders added

---
+++ config/orm.js
@@ -0,0 +1,52 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table + " (" + cols + ")" + "VALUES (?) ";
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table + " SET DEVOURED=" + booleanVal + " WHERE " + condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table + " WHERE " + condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+// Export the orm object for the model (burger.js)
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES (mandarin honeysuckle squid ink stilton);
+INSERT INTO burgers (burger_name) VALUES (bolut cheez whiz watermelon);
+INSERT INTO burgers (burger_name) VALUES (mole schweinefleisch nattou pomegranate);
